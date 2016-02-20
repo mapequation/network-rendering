1c9c8d45d6b0b4e539558632cab2951481af1001
Daniel Edler <edler.dev@gmail.com>
2016-02-20 12:28:15 +0100

    Example with nice colors

--- example/network.js
+++ example/network.js
@@ -4,24 +4,26 @@ var data = {
     {
       x: 100,
       y: 100,
-      r: 50
+      flow: 0.6,
+      outFlow: 0.3
     },
     {
       x: 300,
-      y: 200,
-      r: 20
+      y: 180,
+      flow: 0.4,
+      outFlow: 0.2
     }
   ],
   links: [
     {
       source: 0,
       target: 1,
-      weight: 2
+      flow: 0.5
     },
     {
       source: 1,
       target: 0,
-      weight: 1
+      flow: 0.3
     }
   ]
 };
@@ -37,10 +39,31 @@ data.links[0].oppositeLink = 1;
 data.links[1].oppositeLink = 0;
 
 var width = 400,
-    height = 400;
+    height = 300;
+
+var nodeFillColor = d3.scale.linear()
+  .domain(d3.extent(data.nodes, function(d) { return d.flow; }))
+  .range(["#EF7518", "#D75908"]);
+var nodeBorderColor = d3.scale.linear()
+  .domain(d3.extent(data.nodes, function(d) { return d.outFlow; }))
+  .range(["#FFAE38", "#f9a327"]);
+var nodeRadius = d3.scale.linear()
+  .domain(d3.extent(data.nodes, function(d) { return d.flow; }))
+  .range([20, 30]);
+var nodeBorderWidth = d3.scale.linear()
+  .domain(d3.extent(data.nodes, function(d) { return d.outFlow; }))
+  .range([3, 6]);
+var linkFillColor = d3.scale.linear()
+  .domain(d3.extent(data.links, function(d) { return d.flow; }))
+  .range(["#71B2D7", "#418EC7"]);
+var linkWidth = d3.scale.linear()
+  .domain(d3.extent(data.links, function(d) { return d.flow; }))
+  .range([7, 13]);
 
 var linkRenderer = networkRendering.halfLink()
-  .width(function(d) { return d.weight * 10; })
+  .r0(function(d) { return nodeRadius(d.source.flow); })
+  .r1(function(d) { return nodeRadius(d.target.flow); })
+  .width(function(d) { return linkWidth(d.flow); })
   .oppositeLink(function(d) { return data.links[d.oppositeLink]; });
 
 var svg = d3.select("body").append("svg")
@@ -51,8 +74,8 @@ var links = svg.append("g").selectAll(".link")
     .data(data.links)
   .enter().append("path")
     .attr("class", "link")
-    .style("fill", "none")
-    .style("stroke", "#666")
+    .style("fill", function(d) { return linkFillColor(d.flow); })
+    .style("stroke", "none")
     .style("stroke-width", "1.5px")
     .attr("d", linkRenderer);
 
@@ -60,9 +83,9 @@ var nodes = svg.append("g").selectAll(".node")
     .data(data.nodes)
   .enter().append("circle")
     .attr("class", "node")
-    .style("fill", "#ccc")
-    .style("stroke", "#333")
-    .style("stroke-width", "1.5px")
+    .style("fill", function(d) { return nodeFillColor(d.flow); })
+    .style("stroke", function(d) { return nodeBorderColor(d.outFlow); })
+    .style("stroke-width", function(d) { return nodeBorderWidth(d.outFlow); })
     .attr("cx", function(d) { return d.x; })
     .attr("cy", function(d) { return d.y; })
-    .attr("r", function(d) { return d.r; });
+    .attr("r", function(d) { return nodeRadius(d.flow); });
