80b07389759a6929b97458039c92def53f510437
Anton Eriksson <aner0164@student.umu.se>
2018-02-02 10:06:05 +0100

    Add tests for undirectedLink

--- test/unit/network-rendering.js
+++ test/unit/network-rendering.js
@@ -1,7 +1,7 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
- 
+
 import networkRendering from '../../src/network-rendering';
 
 function checkLinkPath(linkPath) {
@@ -25,6 +25,51 @@ describe('networkRendering', () => {
 
     it('halfLink should have been called once', () => {
       expect(networkRendering.halfLink).to.have.been.calledOnce;
+    });
+
+    it('should return empty link for overlapping nodes', () => {
+      const linkPath = linkRenderer({
+        source: {x: 10, y: 10, size: 10},
+        target: {x: 20, y: 20, size: 10}
+      });
+      expect(linkPath).to.equal('');
+    });
+
+    it('should return non-empty link for overlapping nodes if large bend', () => {
+      linkRenderer.bend(50);
+      const linkPath = linkRenderer({
+        source: {x: 10, y: 10, size: 10},
+        target: {x: 20, y: 20, size: 10}
+      });
+      expect(linkRenderer.bend).to.have.been.calledOnce;
+      expect(linkRenderer.bend()('dummy link')).to.equal(50);
+      checkLinkPath(linkPath);
+      linkRenderer.bend(20);
+      expect(linkRenderer.bend).to.have.been.calledThrice;
+    });
+
+    it('should return a SVG path string for non-overlapping nodes', () => {
+      const linkPath = linkRenderer({
+        source: {x: 10, y: 10, size: 10},
+        target: {x: 30, y: 20, size: 5}
+      });
+      expect(linkPath).to.contain('M');
+      expect(linkPath.length).to.be.within(100, 500);
+    });
+  });
+
+  describe('undirectedLink', () => {
+    var linkRenderer;
+
+    beforeEach(() => {
+      spy(networkRendering, 'undirectedLink');
+      linkRenderer = networkRendering.undirectedLink();
+      spy(linkRenderer, 'width');
+      spy(linkRenderer, 'bend');
+    });
+
+    it('undirectedLink should have been called once', () => {
+      expect(networkRendering.undirectedLink).to.have.been.calledOnce;
     });
 
     it('should return empty link for overlapping nodes', () => {
